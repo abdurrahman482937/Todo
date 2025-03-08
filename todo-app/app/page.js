69468dae4758f947b582594bed15981028b153d9
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-03-08 18:36:21 +0600

    half complete

--- todo-app/app/page.js
+++ todo-app/app/page.js
@@ -4,18 +4,34 @@ import React, { useState } from "react";
 export default function Page() {
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
-  const [close, setClose] = useState("close");
+  const [mainTask, setMainTask] = useState([]);
 
   const submitHandle = (e) => {
     e.preventDefault();
     console.log("I Love You Raisa 😘");
   };
+  let renderTask = <h2 className="mt-10 h-20 pl-67 bg-zinc-100 flex items-center text-2xl font-mono ">No Task Available</h2>;
+  if (mainTask.length > 0) 
+    renderTask = mainTask.map((task, i) => {
+    return (
+      <div
+        key={i}
+        className="mt-10 h-20 px-20 bg-zinc-100 flex justify-between items-center text-2xl font-mono "
+      >
+        <h3>{task.title}</h3>
+        <h3>{task.desc}</h3>
+      </div>
+    );
+  });
   return (
     <div className="">
       <h1 className="bg-zinc-900 text-zinc-100 text-center text-5xl font-semibold font-mono py-5 px-10">
         Todo App
       </h1>
-      <form className="flex items-center justify-evenly" onSubmit={submitHandle}>
+      <form
+        className="flex items-center justify-evenly"
+        onSubmit={submitHandle}
+      >
         <input
           className=" text-xl font-mono border-4 border-zinc-800 text-zinc-950 outline-0 h-12 w-80 px-2 my-10 mx-5"
           type="text"
@@ -36,18 +52,17 @@ export default function Page() {
         />
         <button
           className="bg-zinc-800 text-white text-lg px-12 h-12 cursor-pointer"
-          onClick={(e)=>{
-            set
+          onClick={() => {
+            setMainTask([...mainTask, { title, desc }]);
+            setTitle("");
+            setDesc("");
           }}
         >
           Add Task
         </button>
       </form>
-      <div className="flex items-center justify-evenly" >
-        <h1>{title}</h1>
-        <h1>{desc}</h1>
-        <h1>close</h1>
-      </div>
+      <hr />
+      <div className="my-10 h-auto">{renderTask}</div>
     </div>
   );
 }
