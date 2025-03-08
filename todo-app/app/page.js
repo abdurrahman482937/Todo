42a968289d969aa56313d1352873c46e4829f8ab
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-03-08 19:05:15 +0600

    Full Done!

--- todo-app/app/page.js
+++ todo-app/app/page.js
@@ -10,19 +10,38 @@ export default function Page() {
     e.preventDefault();
     console.log("I Love You Raisa 😘");
   };
-  let renderTask = <h2 className="mt-10 h-20 pl-67 bg-zinc-100 flex items-center text-2xl font-mono ">No Task Available</h2>;
-  if (mainTask.length > 0) 
+
+  const deleteHandler = (i) => {
+    let copyTask = [...mainTask];
+    copyTask.splice(i, 1);
+    setMainTask(copyTask)
+  };
+
+  let renderTask = (
+    <h2 className="mt-5 h-20 bg-zinc-100 flex justify-center items-center text-2xl font-mono ">
+      No Task Available
+    </h2>
+  );
+
+  if (mainTask.length > 0)
     renderTask = mainTask.map((task, i) => {
-    return (
-      <div
-        key={i}
-        className="mt-10 h-20 px-20 bg-zinc-100 flex justify-between items-center text-2xl font-mono "
-      >
-        <h3>{task.title}</h3>
-        <h3>{task.desc}</h3>
-      </div>
-    );
-  });
+      return (
+        <div
+          key={i}
+          className="mt-5 h-20 px-20 bg-zinc-100 flex justify-evenly items-center text-2xl font-mono "
+        >
+          <h3 className="w-10">{task.title}</h3>
+          <h3 className="w-10 text-xl">{task.desc}</h3>
+          <h3
+            className="px-15 py-2 bg-red-500 text-white cursor-pointer"
+            onClick={deleteHandler}
+          >
+            close
+          </h3>
+        </div>
+      );
+    });
+
   return (
     <div className="">
       <h1 className="bg-zinc-900 text-zinc-100 text-center text-5xl font-semibold font-mono py-5 px-10">
@@ -62,7 +81,14 @@ export default function Page() {
         </button>
       </form>
       <hr />
-      <div className="my-10 h-auto">{renderTask}</div>
+      <div className="my-5 h-auto">
+        <div className="h-20 px-20 bg-zinc-100 flex justify-center items-center text-2xl font-mono ">
+          <h2>Title</h2>
+          <h2 className=" px-50 mx-50">Description</h2>
+          <h2>Button</h2>
+        </div>
+      </div>
+      <div className="my-5 h-auto">{renderTask}</div>
     </div>
   );
 }
